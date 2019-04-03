6eb5f1eea958a2e50aa50fd52073e4170b4bee0a
Sukant Priyadarshi <sukant_p@rollingarrays.com>
2019-04-03 08:42:03 +0530

    UPDATE:Key added

--- config/settings/production.py
+++ config/settings/production.py
@@ -4,7 +4,10 @@ from .base import env
 # GENERAL
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
-SECRET_KEY = env("DJANGO_SECRET_KEY")
+SECRET_KEY = env(
+    "DJANGO_SECRET_KEY",
+    default="FhW4UgBwucbAo8Nkvb38sqSpNOxsrJhrSuzjgQJybh06g81locWMDODQZpvfZWgY",
+)
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["example.com"])
 
